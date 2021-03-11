48fd9438e0aeca300e9e00dc7b65acb8fdc132cd
Srishti Ahlawat <67790510+Srishti013@users.noreply.github.com>
2021-03-11 23:41:27 +0530

    Create Day-16_GeekAndHisMarks.cpp

---
+++ Day-16_GeekAndHisMarks.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std; 
+
+
+int32_t main()
+{
+     
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n, x;
+        cin >> n >> x;
+
+        int arr[n];
+        for(int i=0;i<n;i++){
+            cin >> arr[i];
+        }
+
+        int ans = 0;
+        for(int i=0;i<n;i++)
+            if(arr[i]>x)
+                ans++;
+
+        cout << ans << "\n";
+    }
+
+    return 0;
+
+} 
